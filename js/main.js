c202cadbdb6b29f57511cd98060eb39e5646a8a6
mew <mewchildy@gmail.com>
2021-08-18 21:46:40 +0700

    add animations to hero

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ PlayState = {};
 function Hero(game, x, y) {
   // call Phaser.Sprite constructor
   Phaser.Sprite.call(this, game, x, y, "hero");
+  this.animations.add("stop", [0]);
+  this.animations.add("run", [1, 2], 8, true); // 8fps looped
+  this.animations.add("jump", [3]);
+  this.animations.add("fall", [4]);
 
   this.anchor.set(0.5, 0.5);
   this.game.physics.enable(this);
@@ -14,7 +18,14 @@ Hero.prototype.constructor = Hero;
 Hero.prototype.move = function (direction) {
   const SPEED = 300;
   this.body.velocity.x = direction * SPEED;
+
+  if (this.body.velocity.x < 0) {
+    this.scale.x = -1;
+  } else if (this.body.velocity.x > 0) {
+    this.scale.x = 1;
+  }
 };
+
 Hero.prototype.jump = function () {
   const JUMP_SPEED = 600;
   let canJump = this.body.touching.down;
@@ -28,6 +39,31 @@ Hero.prototype.jump = function () {
 Hero.prototype.bounce = function () {
   const BOUNCE_SPEED = 200;
   this.body.velocity.y = -BOUNCE_SPEED;
+};
+
+Hero.prototype._getAnimationName = function () {
+  let name = "stop"; // default animation
+
+  // jumping
+  if (this.body.velocity.y < 0) {
+    name = "jump";
+  }
+  // falling
+  else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
+    name = "fall";
+  } else if (this.body.velocity.x !== 0 && this.body.touching.down) {
+    name = "run";
+  }
+
+  return name;
+};
+
+Hero.prototype.update = function () {
+  // update sprite animation, if it needs changing
+  let animationName = this._getAnimationName();
+  if (this.animations.name !== animationName) {
+    this.animations.play(animationName);
+  }
 };
 
 function Spider(game, x, y) {
@@ -82,7 +118,7 @@ PlayState.preload = function () {
   this.game.load.image("grass:2x1", "images/grass_2x1.png");
   this.game.load.image("grass:1x1", "images/grass_1x1.png");
 
-  this.game.load.image("hero", "images/hero_stopped.png");
+  this.game.load.spritesheet("hero", "images/hero.png", 36, 42);
 
   this.game.load.image("invisible-wall", "images/invisible_wall.png");
 
